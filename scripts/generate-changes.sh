1771ad7f1afe4a42fd2dd786078ca64b5e2dcf62
Jonathan Perkin <jperkin@pkgsrc.org>
2025-04-03 13:17:51 +0100

    actually use start and end

--- scripts/generate-changes.sh
+++ scripts/generate-changes.sh
@@ -4,7 +4,10 @@
 if [ -n "${GIT_COMMIT}" -a -n "${GIT_PREVIOUS_COMMIT}" ]; then
 	start=${GIT_PREVIOUS_COMMIT}
 	end=${GIT_COMMIT}
+else
+	echo "Could not determine start and end commits, aborting."
+	exit 1
 fi
 
-git log --name-status --pretty=format:'%H %an' \
-    | awk -f $(dirname $0)/calculate-changes.awk
+git log --name-status --pretty=format:'%H %an' ${start}..${end} \
+    | awk -f $(dirname $0)/calculate-changes.awk >${WORKSPACE_TMP}/changes.txt
